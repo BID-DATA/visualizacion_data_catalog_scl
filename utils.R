70a69257c978e10d3edd85397df512cb110e6ed1
rsanchezavalos <r.sanchezavalos@gmail.com>
2019-09-30 11:17:36 -0400

    add installation utils

---
+++ utils.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+########################################
+## Instalación y preparación de Ambiente
+########################################
+
+# Instalar los paquetes usados en este repositorio
+instalar <- function(paquete) {
+  if (!require(paquete,character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE)) {
+    install.packages(as.character(paquete), repos = "http://cran.us.r-project.org")
+    library(paquete, character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE)
+  }
+}
+
+paquetes <- c('ggplot2','tidyverse','plyr','scales','knitr', 
+              'kableExtra', 'ggmap','ROAuth', 'twitteR',
+              'devtools', 'rtweet', 'igraph', 'tidygraph',
+              'ggraph', 'networkD3','seriation', 'influenceR',
+              "jsonlite", "stringi")
+
+lapply(paquetes, instalar)
+
+
